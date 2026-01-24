2e98b2a577d768b9c594716829045131118144e5
Manjeet Kumar Mishra <mishramanjeet26@gmail.com>
2026-01-25 00:30:09 +0530

    Langchain project

--- RAG/index.py
+++ RAG/index.py
@@ -1,5 +1,10 @@
+# filepath: c:\Users\DELL\OneDrive\Desktop\AgenticAI\RAG\index.py
 from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
 
 pdf_path = Path(__file__).parent / "story.pdf"
 
-loader = PyPDFLoader(file_path=pdf)
+loader = PyPDFLoader(file_path=pdf_path)
+docs = loader.load()
+
+print(docs[12])
