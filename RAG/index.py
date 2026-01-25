4e20932dcc3aa9140f734b250d9e7a9345ad94fd
Manjeet Kumar Mishra <mishramanjeet26@gmail.com>
2026-01-25 23:20:01 +0530

    Initial commit

--- RAG/index.py
+++ RAG/index.py
@@ -1,7 +1,7 @@
 # filepath: c:\Users\DELL\OneDrive\Desktop\AgenticAI\RAG\index.py
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
-
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 pdf_path = Path(__file__).parent / "story.pdf"
 
 loader = PyPDFLoader(file_path=pdf_path)
