5f6d60d06ce3e3afcf8e77e93a1e394df11984fc
Manjeet Kumar Mishra <mishramanjeet26@gmail.com>
2026-02-14 12:09:47 +0530

    workers

--- rag_queue/queues/workers.py
+++ rag_queue/queues/workers.py
@@ -1,13 +1,17 @@
-from openai import OpenAI
+from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings
 from langchain_qdrant import QdrantVectorStore
+from openai import OpenAI
 
 
+load_dotenv()
 openai_client = OpenAI()
+
 
 embedding_model = OpenAIEmbeddings(
     model = "text-embedding-3-large"
 )
+
 
 vector_db = QdrantVectorStore.from_existing_collection(
     url="http://localhost:6333",
@@ -16,6 +20,49 @@ vector_db = QdrantVectorStore.from_existing_collection(
 )
 
 def process_query(query:str):
-    print("Searching Chunks", query)
-    search_result = vector_db.similarity_search(query=user_query)
-    context = "\n\n\n".join([f"Page content"])
+    print("Searching Chunks",query)
+    search_result = vector_db.similarity_search(query=query)
+    context = "\n\n".join(
+    [
+        f"Page Content: {result.page_content}\n"
+        f"Page Number: {result.metadata['page_label']}\n"
+        f"File Location: {result.metadata['source']}"
+        for result in search_result
+    ]
+)
+    SYSTEM_PROMPT = f"""
+You are a helpful AI Assistant who answers user query based on the available
+context retrieved from a PDF file along with page_contents and page number.
+
+You should only answer the user based on the following context and navigate the
+user to open the right page number to know more.
+
+Context:
+{context}
+"""
+    response = openai_client.chat.completions.create(
+    model="gpt-4.1-mini",
+    messages=[
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": query},
+    ]
+    )
+    print(f"🤖: {response.choices[0].message.content}")
+    return response.choices[0].message.content
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
