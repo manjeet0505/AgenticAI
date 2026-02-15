f5e2b46ef1e74658c6bc440f4dd2d89973f03c12
Manjeet Kumar Mishra <mishramanjeet26@gmail.com>
2026-02-15 23:57:43 +0530

    Lamgraph learning

--- langraph_learn/chat.py
+++ langraph_learn/chat.py
@@ -1,13 +1,26 @@
 from typing_extensions import TypedDict
 from typing import Annotated
 from langgraph.graph.message import add_messages
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph,START,END
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 def chatbot(state: State):
+    print("\n\nInside the chatbot",state)
     return {"message": ["Hi ,this is message from chatbot node"]}
 def samplenode(state: State):
+    print("\n\nInside the samplenode",state)
     return {"message": ["sample node appended"]}
 graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot",chatbot)
+graph_builder.add_node("samplenode",samplenode)
+
+
+graph_builder.add_edge(START,"chatbot")
+graph_builder.add_edge("chatbot","samplenode")
+graph_builder.add_edge("samplenode",END)
+
+graph = graph_builder.compile()
+updated_state = graph.invoke(State({"messages":["Hi,My name is manjeet mishra"]}))
+print("\n\nupdated State",updated_state)
