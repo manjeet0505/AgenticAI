a89c279da907780d60977411e2c4acd1b98145f6
Manjeet Kumar Mishra <mishramanjeet26@gmail.com>
2026-03-03 13:19:15 +0530

    mem0ai

---
+++ mem_agent/mem.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+from mem0 import Memory
+import os
+from openai import OpenAI
+load_dotenv()
+
+client = OpenAI()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+config = {
+    "version": "v1.1",
+    "embedder": {
+        "provider": "openai",
+        "config": { 
+            "api_key": OPENAI_API_KEY, 
+            "model": "text-embedding-3-small" 
+        }
+    },
+    "llm": {
+        "provider": "openai",
+        "config": { 
+            "api_key": OPENAI_API_KEY, 
+            "model": "gpt-4.1" 
+        }
+    },
+    "vector_store": {
+        "provider" : "qdrant",
+        "config" : {
+            "host" : "localhost",
+            'port' : 6333
+        }
+    }
+}
+
+mem_client = Memory.from_config(config)
